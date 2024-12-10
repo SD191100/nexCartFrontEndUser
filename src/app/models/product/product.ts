2840462a576830e3f9749eb735680830fc443b6e
SD191100 <shivamdurgude1@gmail.com>
2024-12-10 17:50:54 +0530

    created some components

---
+++ src/app/models/product/product.ts
@@ -0,0 +1,14 @@
+export interface Product {
+ category: null,
+    categoryId: number,
+    description: string,
+    name: string,
+    orderDetails: null,
+    price: number,
+    productId: number,
+    productInventory: null,
+    seller: null,
+    sellerId: number,
+    stock: number,
+
+};
